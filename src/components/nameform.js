60794828c030d269e0246f84341aac449b9467d4
Jen Kagan <jen@netlify.com>
2020-03-25 15:43:46 -0700

    Add form component - add 'thank you' success page

---
+++ src/components/nameform.js
@@ -0,0 +1,50 @@
+// cobbled together from:
+// https://github.com/futuregerald/react-netlify-form-file/
+// https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
+
+import React, { useState } from "react";
+
+const NameForm = (props) => {
+  const [name, setName] = useState("");
+
+  const createFormDataObj = (data) => {
+    const formData = new FormData();
+    Object.keys(data).forEach((k)=>{
+      formData.append(k,data[k])
+    });
+    return formData
+  }
+  
+  const handleSubmit = (e) => {
+      const data = { 
+        "form-name": "contact",
+        "name": name
+      }
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(createFormDataObj(data)).toString()
+      })
+        .then(() => alert(`Submitting Name ${name}`))
+        .catch(error => alert(error));
+      
+      e.preventDefault();
+  };
+
+  return (
+    <form data-netlify="true" action="/" name="contact" method="post" onSubmit={handleSubmit}>
+      <label>
+        Frirst Name:
+        <input
+          name="fname"
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default NameForm
---
+++ src/pages/thank-you.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
+
+const ThanksPage = () => (
+  <Layout>
+    <h1>THANK YOU</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default ThanksPage
