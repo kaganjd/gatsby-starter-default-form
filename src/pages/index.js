4ccd833d693f27826f9b29d957e47d948066e9c1
Jen Kagan <jen@netlify.com>
2020-10-10 16:46:15 -0700

    Refactor
    - improve comments, encoding function, and use of useState
    - reference Swyx's example at the top

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import NameForm from "../components/nameform"
+import NameForm from "../components/form"
 
 const IndexPage = () => (
   <Layout>
