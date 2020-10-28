161612bf018dfae8b6adc60d69c8dcf6c5e2aa45
Jen Kagan <jen@netlify.com>
2020-10-28 14:48:48 -0700

    test form action and removing navigate()

--- src/components/form.js
+++ src/components/form.js
@@ -4,29 +4,27 @@
 
 // This is what we'll use to navigate to the custom success page
 // More on this here: https://www.gatsbyjs.org/docs/gatsby-link/#how-to-use-the-navigate-helper-function
-import { navigate } from "gatsby"
-import React, { useState } from "react";
+import React, { useState } from "react"
 
 // This function encodes the captured form data in the format that Netlify's backend requires
 function encode(data) {
     return Object.keys(data)
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
+        .join("&")
   }
 
 const NameForm = (props) => {
   const [name, setName] = useState("")
 
-  const handleChange = (e) => {
-    setName({ ...name, [e.target.name]: e.target.value })
+  const handleChange = (event) => {
+    setName({ ...name, [event.target.name]: event.target.value })
   }
 
+  // POST the encoded form with the content-type header that's required for a text submission
+  // Note that the header will be different for POSTing a file
   const handleSubmit = (event) => {
-    // Prevent the default onSubmit behavior
-    event.preventDefault();
-    // POST the encoded form with the content-type header that's required for a text submission
-    // Note that the header will be different for POSTing a file
-    fetch("/", {
+    event.preventDefault()
+    fetch("/butts", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ 
@@ -34,26 +32,19 @@ const NameForm = (props) => {
         ...name
       })
     })
-      // On success, redirect to the custom success page using Gatsby's `navigate` helper function
-      .then(() => navigate("/thank-you/"))
-      // On error, show the error in an alert
-      .catch(error => alert(error));
-  };
+    .then(() => console.log('nice'))
+    .catch(error => alert(error))
+  }
 
   return (
-    <form data-netlify="true" action="/" name="contact-form" method="post" onSubmit={handleSubmit}>
+    <form data-netlify="true" action="/thank-you" name="contact-form" method="post" onSubmit={handleSubmit}>
       <input type="hidden" name="form-name" value="contact-form" />
-      <label>
-        Name:
-        <input
-          name="firstName"
-          type="text"
-          onChange={handleChange}
-        />
+      <label>Name:
+        <input name="firstName" type="text" onChange={handleChange} />
       </label>
       <input type="submit"/>
     </form>
-  );
+  )
 }
 
 export default NameForm
